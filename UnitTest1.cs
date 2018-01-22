0049a92a0c02937e3d6990d007ee25b07e3dd36d
GeorgeLincoln <georgelincoln1996@gmail.com>
2018-01-22 21:24:09 -0200

    Implementação de teste unitário

---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using bll;
+
+namespace unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly Operacao operacao = new Operacao();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            operacao.Somar(a: 1, b: 2);
+            operacao.Subtrair(a: 1, b: 2);
+            operacao.Multiplicar(a: 1, b: 2);
+            operacao.Dividir(a: 1, b: 2);
+        }
+    }
+}
