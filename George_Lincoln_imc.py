018f87186b1808d89633a3c40362d01c2f006dc5
GeorgeLincoln <georgelincoln1996@gmail.com>
2018-01-22 23:06:40 -0200

    Programa imc.py

---
+++ George_Lincoln_imc.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: ")
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+imc = peso / (altura*altura)
+
+if imc < 18.4:
+    print("Abaixo do peso")
+if imc >= 18.5 and imc < 24.9:
+    print("Peso normal")
+else:
+    print("Acima do peso") 
